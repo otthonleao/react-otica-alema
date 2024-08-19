fee24cb6c5a1c57e237fd7afd603980bfe75c8fd
otthonleao <otthonleao@hotmail.com>
2024-08-19 01:19:09 -0400

    fix: change background and text-color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="mt-0 text-white font-bold text-2xl">Ótica Alemã</h1>
-      <h2 className="md:text-5xl text-center">🚧 Página em Construção 🚧</h2>
-      <h3 className="mb-10 text-white font-bold text-2xl">Muraton
-        <span className="bg-gradient-to-r from-blue-400 to-blue-300 bg-clip-text text-transparent">Software</span>
-      </h3>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
+			<h1 className="mt-0  font-bold text-2xl">Ótica Alemã</h1>
+			<h2 className="md:text-5xl text-center">🚧 Página em Construção 🚧</h2>
+			<h3 className="mb-10 font-bold text-1xl">Muraton
+				<span className="bg-gradient-to-r from-blue-400 to-blue-300 bg-clip-text text-transparent">Software</span>
+			</h3>
+		</main>
+	);
 }
